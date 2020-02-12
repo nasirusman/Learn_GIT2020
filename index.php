21578409d6e98ced8b121bde38fa14fe0e535447
Nasir Usman <nasirusman92@gmail.com>
2020-02-12 19:59:11 +0800

    Penambahan latar belakang

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="assets/css/styles.css">
+
     <style>
         body {
             padding: 30px 0;
@@ -17,6 +20,8 @@
             max-width: 700px;
             margin: auto;
             margin-top: 20px;
+            background-color: #ffffff;
+            padding-bottom: 2px;
         }
 
         .my-table img {
